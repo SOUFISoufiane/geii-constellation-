0a219a318a3027d459ca75f23dcaec2bcb56721f
ASsouf <soufisoufiane21@gmail.com>
2026-05-30 20:52:57 +0200

    chore: clean up repo, remove personal and backup files

--- Maths/backups/legacy-v5.2/test_import.mjs
+++
@@ -1,7 +0,0 @@
-global.window = global;
-global.document = { body: {} };
-global.window.Plotly = {};
-global.window.renderMathInElement = () => {};
-global.MathJax = {};
-
-import('./js/main.js').then(() => console.log('Import successful!')).catch(e => console.error('Import failed:', e));
